04d037248e1b6784fdea15347b761f6031a77e42
morkoto <tmorkovsky@spork.tech>
2021-03-25 12:51:17 +0100

    HEADER componet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from "./pages/homepage/Homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 
 
 
@@ -12,6 +12,7 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
