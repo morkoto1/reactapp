c924b9d65d0380fcf8753bbb1bf8c383a0b50e03
morkoto <tmorkovsky@spork.tech>
2021-03-27 12:36:24 +0100

    SIGN IN component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from "./pages/homepage/Homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
-
 
 
 
@@ -16,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInPage} />
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,6 +17,9 @@ const Header = () => (
             <Link className='option' to='/contant'>
                 CONTACT
             </Link>
+            <Link className='option' to='/signin'>
+                SIGN IN
+            </Link>
         </div>
     </div>
 );
